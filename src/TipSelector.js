04add5560b7be0164372302c8b685582ef3b306f
Vishesh Dadhich <visheshdadhich20@gmail.com>
2024-06-15 19:07:13 +0530

    Fixed a minor Tip Bug

--- src/TipSelector.js
+++ src/TipSelector.js
@@ -3,7 +3,7 @@ export default function TipSelector({ children, tip, onTipSelection }) {
         <div>
             <label>{children}</label>
             <select value={tip} onChange={onTipSelection}>
-                <option value={0}>No Tip (0%)</option>
+                <option value={0} selected>No Tip (0%)</option>
                 <option value={5}>Decent (5%)</option>
                 <option value={10}>Satisfied (10%)</option>
                 <option value={15}>Excellent (15%)</option>
--- src/TotalBill.js
+++ src/TotalBill.js
@@ -2,7 +2,7 @@ export default function TotalBill({ tip1, tip2, bill }) {
     const totalTip = Math.floor(((tip1 + tip2) / 100) * bill);
     return (
         <div>
-            <h3>Total Bill is : {bill + totalTip}$ ({bill} +{totalTip})</h3>
+            <h3>Total Bill is : {bill + totalTip} $ ({bill} +{totalTip})</h3>
         </div>
     )
 }
